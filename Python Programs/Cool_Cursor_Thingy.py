9d4ea6722b9d79e98dbf159a7d37789336608faa
ComputerKWasTaken <computerk1337@gmail.com>
2023-12-13 11:09:45 -0600

    Adjusted circle positions and the works

--- Python Programs/Cool_Cursor_Thingy.py
+++ Python Programs/Cool_Cursor_Thingy.py
@@ -11,17 +11,26 @@ CIRCLE_COLOR = pygame.Color('white')
 MAX_DISTANCE = math.sqrt(WIDTH**2 + HEIGHT**2)
 
 # Create the window
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)  # Make the window resizable
 
 # Set the number of circles
 num_circles = 10  # Change this value for testing
 
 # Create a list of circles
-circles = [(i * WIDTH // num_circles, j * HEIGHT // num_circles) for i in range(num_circles) for j in range(num_circles)]
+circles = [(i * WIDTH // (num_circles-1), j * HEIGHT // (num_circles-1)) for i in range(num_circles) for j in range(num_circles)]
 
 # Game loop
 running = True
 while running:
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.VIDEORESIZE:  # The window has been resized
+            WIDTH, HEIGHT = event.w, event.h
+            MAX_DISTANCE = math.sqrt(WIDTH**2 + HEIGHT**2)
+            circles = [(i * WIDTH // (num_circles-1), j * HEIGHT // (num_circles-1)) for i in range(num_circles) for j in range(num_circles)]
+
     # Fill the background
     window.fill(BACKGROUND_COLOR)
 
@@ -34,18 +43,13 @@ while running:
         distance = math.sqrt((x - mouse_pos[0])**2 + (y - mouse_pos[1])**2)
 
         # Calculate the color based on the distance
-        color = int(255 * (1 - distance / MAX_DISTANCE))  # Circles become brighter when the cursor is closer
+        color = int(255 * (1 - (distance / MAX_DISTANCE)**2))  # Increase the magnitude of the inverse square law
 
         # Draw the circle
         pygame.draw.circle(window, (color, color, color), (x, y), 20)
 
     # Update the display
     pygame.display.flip()
-
-    # Event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
 # Quit Pygame
 pygame.quit()
