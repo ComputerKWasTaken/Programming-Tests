fb25e7c6744cc815a77911d73ad34dc2089efa4f
ComputerKWasTaken <computerk1337@gmail.com>
2023-12-15 10:35:14 -0600

    Just some adjustments, nothing special

--- Python Programs/Cool_Cursor_Thingy.py
+++ Python Programs/Cool_Cursor_Thingy.py
@@ -14,7 +14,7 @@ MAX_DISTANCE = math.sqrt(WIDTH**2 + HEIGHT**2)
 window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)  # Make the window resizable and constraint it to the screen
 
 # Set the number of circles
-num_circles = 20 # Adjust this value to change the number of circles
+num_circles = 100 # Adjust this value to change the number of circles
 
 # Create a list of circles
 circles = [(i * WIDTH // (num_circles-1), j * HEIGHT // (num_circles-1)) for i in range(num_circles) for j in range(num_circles)]
@@ -44,7 +44,7 @@ while running:
         distance = math.sqrt((x - mouse_pos[0])**2 + (y - mouse_pos[1])**2)
 
         # Calculate the color based on the distance
-        color = min(int(255 * (1 - (distance / MAX_DISTANCE)**0.5)), 255)  # Ensure color does not exceed 255
+        color = min(int(255 * (1 - (distance / MAX_DISTANCE)**0.1)), 255)  # Ensure color does not exceed 255
 
         # Draw the circle
         pygame.draw.circle(window, (color, color, color), (x, y), 20)
